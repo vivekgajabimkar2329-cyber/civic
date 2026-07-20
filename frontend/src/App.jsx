126c12a008bf156fe9f97bd4facec1c8fd51da39
vivekgajabimkar2329-cyber <vivekgajabimkar2329@gmail.com>
2026-07-20 15:26:43 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Civic Frontend</h1>
+      <p>Your Vite setup is now working!</p>
+    </div>
+  );
+}
+
+export default App;
